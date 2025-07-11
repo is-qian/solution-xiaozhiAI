071597e817cf57d8277ad8bc6a17a03397650e8e
陈志和 <aaron.chen@QUECTEL.COM>
2025-07-11 17:35:36 +0800

    增加led灯控

--- src/_main.py
+++ src/_main.py
@@ -3,22 +3,86 @@
 """
 import utime
 import gc
-from machine import ExtInt
+from machine import ExtInt,Pin
 from usr.protocol import WebSocketClient
 from usr.utils import ChargeManager, AudioManager, NetManager, TaskManager
-from usr.threading import Thread, Event
+from usr.threading import Thread, Event, Condition
 from usr.logging import getLogger
 
 
 logger = getLogger(__name__)
 
+class Led(object):
+
+    def __init__(self, GPIOn):
+        self.__led = Pin(
+            getattr(Pin, 'GPIO{}'.format(GPIOn)),
+            Pin.OUT,
+            Pin.PULL_PD,
+            0
+        )
+        self.__off_period = 1000
+        self.__on_period = 1000
+        self.__count = 0
+        self.__running_cond = Condition()
+        self.__blink_thread = None
+        self.off()
+
+    @property
+    def status(self):
+        with self.__running_cond:
+            return self.__led.read()
+
+    def on(self):
+        with self.__running_cond:
+            self.__count = 0
+            return self.__led.write(0)
+
+    def off(self):
+        with self.__running_cond:
+            self.__count = 0
+            return self.__led.write(1)
+
+    def blink(self, on_period=50, off_period=50, count=None):
+        if not isinstance(count, (int, type(None))):
+            raise TypeError('count must be int or None type')
+        with self.__running_cond:
+            if self.__blink_thread is None:
+                self.__blink_thread = Thread(target=self.__blink_thread_worker)
+                self.__blink_thread.start()
+            self.__on_period = on_period
+            self.__off_period = off_period
+            self.__count = count
+            self.__running_cond.notify_all()
+
+    def __blink_thread_worker(self):
+        while True:
+            with self.__running_cond:
+                if self.__count is not None:
+                    self.__running_cond.wait_for(lambda: self.__count is None or self.__count > 0)
+                status = self.__led.read()
+                self.__led.write(1 - status)
+                utime.sleep_ms(self.__on_period if status else self.__off_period)
+                self.__led.write(status)
+                utime.sleep_ms(self.__on_period if status else self.__off_period)
+                if self.__count is not None:
+                    self.__count -= 1
 
 class Application(object):
 
     def __init__(self):
         # 初始化唤醒按键
-        self.talk_key = ExtInt(ExtInt.GPIO19, ExtInt.IRQ_FALLING, ExtInt.PULL_PU, self.on_talk_key_click, 50)
-
+        self.talk_key = ExtInt(ExtInt.GPIO19, ExtInt.IRQ_RISING_FALLING, ExtInt.PULL_PU, self.on_talk_key_click, 50)
+        
+        # 初始化 led; write(1) 灭； write(0) 亮
+        self.wifi_red_led = Led(33)
+        self.wifi_green_led = Led(32)
+        self.power_red_led = Led(39)
+        self.power_green_led = Led(38)
+        self.lte_red_led = Led(23)
+        self.lte_green_led = Led(24)
+        self.led_power_pin = Pin(Pin.GPIO27, Pin.OUT, Pin.PULL_DISABLE, 0)
+        
         # 初始化充电管理
         self.charge_manager = ChargeManager()
 
@@ -83,6 +147,7 @@ class Application(object):
         self.start_vad()
         try:
             with self.__protocol:
+                self.power_red_led.on()
                 self.__protocol.hello()
                 self.__protocol.wakeword_detected("小智")
                 is_listen_flag = False
@@ -105,6 +170,7 @@ class Application(object):
         except Exception as e:
             logger.debug("working thread handler got Exception: {}".format(repr(e)))
         finally:
+            self.power_red_led.blink(250, 250)
             self.stop_vad()
 
     def on_talk_key_click(self, args):
@@ -144,7 +210,14 @@ class Application(object):
     def handle_stt_message(data, msg):
         raise NotImplementedError("handle_stt_message not implemented")
 
-    def handle_tts_message(data, msg):
+    def handle_tts_message(self, msg):
+        state = msg["state"]
+        if state == "start":
+            self.wifi_green_led.blink(250, 250)
+        elif state == "stop":
+            self.wifi_green_led.off()
+        else:
+            pass
         raise NotImplementedError("handle_tts_message not implemented")
 
     def handle_llm_message(data, msg):
@@ -158,7 +231,8 @@ class Application(object):
         self.audio_manager.open_opus()
         self.talk_key.enable()
         self.start_kws()
-
+        self.led_power_pin.write(1)
+        self.power_red_led.blink(250, 250)
 
 if __name__ == "__main__":
     app = Application()
